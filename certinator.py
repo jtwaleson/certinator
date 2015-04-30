d8928ce4e1cfdcb7715308f247dfb870f2681307
Jouke Waleson <jouke@waleson.com>
2015-04-30 16:14:37 +0200

    bump experimentation phase

--- certinator.py
+++ certinator.py
@@ -1,14 +1,21 @@
-#!/usr/bin/python
-
 import subprocess
 from OpenSSL import crypto
 from Crypto.Util import asn1
+import sys
+import logging
+from flask import Flask
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    stream=sys.stdout,
+    format='%(levelname)s: %(message)s',
+)
 
 
 def get_certs_from_lines(lines):
-    lines = [line.strip() for line in lines.split('\n')]
     result = []
     for line in lines:
+        line = line.strip()
         if len(result) > 0 or line == '-----BEGIN CERTIFICATE-----':
             result.append(line)
         if line == '-----END CERTIFICATE-----':
@@ -17,7 +24,9 @@ def get_certs_from_lines(lines):
 
 
 def get_cert_from_string(input_string):
-    return crypto.load_certificate(crypto.FILETYPE_PEM, input_string)
+    cert = crypto.load_certificate(crypto.FILETYPE_PEM, input_string)
+    print(cert.digest('sha1'))
+    return cert
 
 
 def get_certs_from_domain(domain, port=443):
@@ -28,38 +37,47 @@ def get_certs_from_domain(domain, port=443):
         '-showcerts',
     ], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = process.communicate(b'')
-    certs = [get_cert_from_string(c) for c in get_certs_from_lines(out.decode())]
-    verify_certificate_to_authority(certs[1], certs[2])
+    for cert_string in get_certs_from_lines(out.decode().split('\n')):
+        yield get_cert_from_string(cert_string)
 
 
-def verify_certificate_to_authority(certificate, authority):
+def certificate_is_signed_by_authority(certificate, authority):
     signature_algorithm = certificate.get_signature_algorithm()
-    # Get the ASN1 format of the certificate
     certificate_asn1 = crypto.dump_certificate(crypto.FILETYPE_ASN1, certificate)
     certificate_der = asn1.DerSequence()
     certificate_der.decode(certificate_asn1)
-    # The certificate has three parts:
-    # - certificate
-    # - signature algorithm
-    # - signature
-    # http://usefulfor.com/nothing/2009/06/10/x509-certificate-basics/
     der_cert, der_algo, der_sig = certificate_der
-
-    # The signature is a BIT STRING (Type 3)
-    # Decode that as well
     der_sig_in = asn1.DerObject()
     der_sig_in.decode(der_sig)
     sig0 = der_sig_in.payload
     if sig0[0] != '\x00':
         raise Exception('Number of unused bits is strange')
-
-    # Now get the signature itself
     sig = sig0[1:]
+    try:
+        crypto.verify(authority, sig, der_cert, signature_algorithm)
+        return True
+    except crypto.Error:
+        return False
+    return False
 
-    # And verify the certificate
-    crypto.verify(authority, sig, der_cert, signature_algorithm)
-    print "Certificate looks good"
+
+def reader(file_handle):
+    last_result = None
+    while last_result != '':
+        last_result = file_handle.readline()
+        yield last_result
 
 
-if __name__ == '__main__':
+def get_certs_from_file(file_name):
+    with open(file_name, 'r') as file_handle:
+        for cert_string in get_certs_from_lines(reader(file_handle)):
+            yield get_cert_from_string(cert_string)
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
     get_certs_from_domain('www.google.com')
+    return 'sffdas'
