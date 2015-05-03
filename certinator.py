4aac0e65ed7c36f445e9ff8a7a60756f7787fbda
Jouke Waleson <jouke@waleson.com>
2015-05-03 16:09:42 +0200

    add chain getting mechanism

--- certinator.py
+++ certinator.py
@@ -48,16 +48,66 @@ def analyze_domain_name(domain, port=443):
     return 'domain checked, thanks for submitting %d new certificates' % added
 
 
-@app.route('/upload', methods=['POST'])
-def upload_certificates():
+def get_certificates_from_request(request):
     files_count = 0
-    cert_count = 0
     for name, uploaded_file in request.files.iteritems():
         files_count += 1
-        for pem_string in certificate_operations.get_pem_strings_from_file_handle(uploaded_file):
-            logging.info('parsed certificates')
-            cert = certificate_operations.get_cert_from_pem_string(pem_string)
-            if warehouse.store(cert):
-                cert_count += 1
+        pem_strings = certificate_operations.get_pem_strings_from_file_handle(
+            uploaded_file
+        )
+        for pem_string in pem_strings:
+            yield (
+                files_count,
+                certificate_operations.get_cert_from_pem_string(pem_string),
+            )
 
-    return 'uploaded %d files, resulting in %d new certificates' % (files_count, cert_count)
+    if files_count == 0:
+        files_count += 1
+        pem_strings = certificate_operations.get_pem_strings_from_lines(
+            request.data.split('\n')
+        )
+        for pem_string in pem_strings:
+            yield (
+                files_count,
+                certificate_operations.get_cert_from_pem_string(pem_string),
+            )
+
+
+@app.route('/upload', methods=['POST'])
+def upload_certificates():
+    file_count = 0
+    cert_count = 0
+    for file_no, cert in get_certificates_from_request(request):
+        file_count = max(file_no, file_count)
+        if warehouse.store(cert):
+            cert_count += 1
+
+    return 'uploaded %d files, resulting in %d new certificates' % (
+        file_count, cert_count
+    )
+
+
+@app.route('/chain/', methods=['POST'])
+def get_chain():
+    likely_certificate = None
+    for file_no, cert in get_certificates_from_request(request):
+        if not certificate_operations.certificate_is_signed_by_authority(
+            cert, cert
+        ):
+            likely_certificate = cert
+            break
+
+    if likely_certificate is None:
+        return 'No certificates found in request', 400
+
+    def response_generator(cert):
+        previous_cert = None
+        # we don't output the last cert from the chain, that is self-signed
+        for chain_cert in certificate_operations.get_certificate_chain(cert):
+            if previous_cert:
+                yield previous_cert
+            previous_cert = certificate_operations.get_pem_string_from_cert(
+                chain_cert
+            )
+
+    return Response(response_generator(likely_certificate))
